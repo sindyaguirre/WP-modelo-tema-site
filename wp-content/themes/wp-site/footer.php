d63cd624111cf141706857e3b16e6819ba2d8e02
Sindy Antunes <sindy.antunes@hammerconsult.com.br>
2019-11-13 16:16:05 -0300

    Atualizando primeiro Hello word

---
+++ wp-content/themes/wp-site/footer.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="<?php bloginfo('stylesheet_url')?>"/>
+        <title> <?php wp_title(); ?></title>
+        <?php wp_head(); ?>
+    </head>
+
+    <body>
